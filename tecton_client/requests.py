3c3674dad7e81e110ccda35829e5bfeb2b780762
Greeshma Karanth <greeshmakaranth.13@gmail.com>
2023-06-07 13:44:04 -0400

    Reordering files

--- tecton_client/requests.py
+++ tecton_client/requests.py
@@ -1,16 +1,102 @@
 import json
 from abc import ABC
 from dataclasses import dataclass
-from typing import Self, Optional, Final, Set
+from enum import Enum
+from types import NoneType
+from typing import Self, Optional, Dict, Union, Set, Final
 
 from tecton_client.exceptions import (
     InvalidParameterMessage,
     InvalidParameterException
 )
-from tecton_client.request.requests_data import (
-    MetadataOptions,
-    GetFeatureRequestData
-)
+
+
+class MetadataOptions(str, Enum):
+    """
+    Options for retrieving metadata for get-features request
+    """
+
+    NAME = "include_names"
+    EFFECTIVE_TIME = "include_effective_times"
+    DATA_TYPE = "include_data_types"
+    SLO_INFO = "include_slo_info"
+    FEATURE_STATUS = "include_serving_status"
+
+    @staticmethod
+    def defaults() -> Set["MetadataOptions"]:
+        """
+        Setting default options to include names and data_types
+        :return: Set["MetadataOptions"]
+        """
+        return {MetadataOptions.NAME, MetadataOptions.DATA_TYPE}
+
+
+@dataclass
+class GetFeatureRequestData:
+    """
+    Class for request data needed for get-features queries
+    """
+
+    def __init__(self: Self,
+                 join_key_map: Optional[Dict[str,
+                 Union[int, str, NoneType]]] = None,
+                 request_context_map: Optional[Dict[str,
+                 Union[int, str, float]]]
+                 = None) -> None:
+        """
+        Constructor that configures join_key_map and request_context_map
+        :param join_key_map: Join keys used for table-based FeatureViews
+        :param request_context_map: Request context used for
+        OnDemand FeatureViews
+        """
+
+        if join_key_map is None and request_context_map is None:
+            raise InvalidParameterException(
+                InvalidParameterMessage.REQUEST_MAPS.value)
+
+        if join_key_map is not None:
+            for key, value in join_key_map.items():
+                if not key or value == "":
+                    raise InvalidParameterException(
+                        InvalidParameterMessage.KEY_VALUE.value)
+
+                if type(key) is not str:
+                    msg = f"{InvalidParameterMessage.TYPE_KEY.value} " \
+                          f"Join Key-Map key: {key}"
+                    raise InvalidParameterException(
+                        msg)
+
+                if type(value) not in (int, str, NoneType):
+                    msg = f"{InvalidParameterMessage.TYPE_JOIN_VALUE.value}" \
+                          f" {value}"
+                    raise InvalidParameterException(
+                        msg)
+
+                join_key_map[key] = str(value) if type(value) is int else value
+
+        if request_context_map is not None:
+            for key, value in request_context_map.items():
+                if not key or not value:
+                    raise InvalidParameterException(
+                        InvalidParameterMessage.KEY_VALUE.value)
+
+                if type(key) is not str:
+                    msg = f"{InvalidParameterMessage.TYPE_KEY.value} " \
+                          f"Request Context Map key: {key}"
+                    raise InvalidParameterException(
+                        msg)
+
+                if type(value) not in (int, str, float):
+                    msg = f"{InvalidParameterMessage.TYPE_REQ_VALUE.value}" \
+                          f" {value}"
+                    raise InvalidParameterException(
+                        msg)
+
+                request_context_map[key] = str(value) if \
+                    type(value) not in (str, float) else value
+
+        self.join_key_map: dict = join_key_map or {}
+        self.request_context_map: dict = request_context_map or {}
 
 
 @dataclass
@@ -55,7 +141,6 @@ class AbstractGetFeaturesRequest(TectonRequest):
     def __init__(self: Self, endpoint: str,
                  workspace_name: str, feature_service_name: str,
                  metadata_options: Set["MetadataOptions"]) -> None:
-
         """
         Constructor that configures the request endpoint, workspace_name,
         feature_service_name and metadata_options
--- tests/requests_test.py
+++ tests/requests_test.py
@@ -1,10 +1,13 @@
 import json
+from typing import Union
+
 import pytest
 
 from tecton_client.exceptions import InvalidParameterException
-from tecton_client.request.requests import GetFeaturesRequest
-from tecton_client.request.requests_data import (
-    GetFeatureRequestData, MetadataOptions
+from tecton_client.requests import (
+    GetFeaturesRequest,
+    GetFeatureRequestData,
+    MetadataOptions
 )
 
 TEST_WORKSPACE_NAME = "test_workspace_name"
@@ -12,6 +15,83 @@ TEST_FEATURE_SERVICE_NAME = "test_feature_service_name"
 
 default_get_feature_request_data = \
     GetFeatureRequestData(join_key_map={"test_key": "test_value"})
+
+
+@pytest.mark.parametrize("key", ["", None])
+def test_error_join_key(key: str) -> None:
+    with pytest.raises(InvalidParameterException):
+        GetFeatureRequestData(join_key_map={key: "test_value"})
+
+
+@pytest.mark.parametrize("key", ["", None])
+def test_error_request_context_key(key: str) -> None:
+    with pytest.raises(InvalidParameterException):
+        GetFeatureRequestData(request_context_map={key: "test_value"})
+
+
+@pytest.mark.parametrize("value", ["", 123.45])
+def test_invalid_join_value(value: Union[str, float]) -> None:
+    with pytest.raises(InvalidParameterException):
+        GetFeatureRequestData(join_key_map={"test_key": value})
+
+
+def test_none_join_value() -> None:
+    get_feature_request_data = \
+        GetFeatureRequestData(join_key_map={"test_key": None})
+    assert get_feature_request_data.join_key_map["test_key"] is None
+
+
+@pytest.mark.parametrize("value", ["", None])
+def test_error_request_context_value(value: object) -> None:
+    with pytest.raises(InvalidParameterException):
+        GetFeatureRequestData(request_context_map={"test_key": value})
+
+
+def test_mixed_type_join_key_value() -> None:
+    get_feature_request_data = GetFeatureRequestData(
+        join_key_map={"test_string_key": "test_string_value",
+                      "test_long_key": 1234})
+    join_key_map = get_feature_request_data.join_key_map
+
+    assert join_key_map is not None
+    assert len(join_key_map) == 2
+    assert join_key_map.get("test_string_key") == "test_string_value"
+    assert join_key_map.get("test_long_key") == "1234"
+
+
+def test_mixed_type_request_context_key_value() -> None:
+    get_feature_request_data = GetFeatureRequestData(request_context_map={
+        "test_string_key": "test_string_value",
+        "test_long_key": 1234,
+        "test_float_key": 123.45
+    })
+
+    request_context_map = get_feature_request_data.request_context_map
+
+    assert request_context_map is not None
+    assert len(request_context_map) == 3
+    assert request_context_map.get("test_string_key") == "test_string_value"
+    assert request_context_map.get("test_long_key") == "1234"
+    assert request_context_map.get("test_float_key") == 123.45
+
+
+def test_join_key_and_request_context() -> None:
+    join_key_map = {"test_join_key_1": "test_join_value_1",
+                    "test_join_key_2": "test_join_value_2"}
+    request_context_map = {"test_request_context_1": 1234,
+                           "test_request_context_2": "test_string_value"}
+
+    get_feature_request_data = GetFeatureRequestData(join_key_map,
+                                                     request_context_map)
+
+    assert get_feature_request_data.join_key_map is not None
+    assert get_feature_request_data.request_context_map is not None
+
+    assert len(get_feature_request_data.join_key_map) == 2
+    assert len(get_feature_request_data.request_context_map) == 2
+
+    assert join_key_map == get_feature_request_data.join_key_map
+    assert request_context_map == get_feature_request_data.request_context_map
 
 
 @pytest.mark.parametrize("workspace", ["", None])
