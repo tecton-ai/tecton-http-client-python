a62b5904b9d6cf2e94e33ca646011e00304820fc
Greeshma Karanth <greeshmakaranth.13@gmail.com>
2023-06-09 14:21:27 -0400

    Adding Data Types

---
+++ tecton_client/utils/data_types.py
@@ -0,0 +1,77 @@
+import abc
+from typing import List
+from typing import Self
+
+
+class DataType(abc.ABC):
+    """
+    Base DataType class. A python utility for working with `tecton_proto.common.DataType` protos.
+    """
+
+    def __str__(self: Self):
+        # Require __str__ implementation. Used in error messages.
+        raise NotImplementedError
+
+
+class IntType(DataType):
+    def __str__(self) -> str:
+        return "Int"
+
+
+class FloatType(DataType):
+    def __str__(self) -> str:
+        return "Float"
+
+
+class StringType(DataType):
+
+    def __str__(self: Self) -> str:
+        return "String"
+
+
+class BoolType(DataType):
+
+    def __str__(self: Self) -> str:
+        return "Bool"
+
+
+class ArrayType(DataType):
+    def __init__(self, element_type: DataType) -> None:
+        self._element_type = element_type
+
+    @property
+    def element_type(self: Self) -> DataType:
+        return self._element_type
+
+    def __str__(self: Self) -> str:
+        return f"Array({self._element_type})"
+
+
+class StructField:
+    def __init__(self, name: str, data_type: DataType) -> None:
+        self._name = name
+        self._data_type = data_type
+
+    @property
+    def name(self: Self) -> str:
+        return self._name
+
+    @property
+    def data_type(self: Self) -> DataType:
+        return self._data_type
+
+    def __str__(self: Self) -> str:
+        return f"Field({self._name}, {self._data_type})"
+
+
+class StructType(DataType):
+    def __init__(self, fields: List[StructField]) -> None:
+        self._fields = fields
+
+    @property
+    def fields(self: Self) -> List[StructField]:
+        return self._fields
+
+    def __str__(self: Self) -> str:
+        fields_string = ", ".join(str(field) for field in self._fields)
+        return f"Struct({fields_string})"
