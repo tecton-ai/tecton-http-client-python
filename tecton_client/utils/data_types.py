66b61900b3e4a123f4e7452042ffc4d0d43955d2
Greeshma Karanth <greeshmakaranth.13@gmail.com>
2023-06-12 18:21:55 -0400

    Adding docstrings

--- tecton_client/utils/data_types.py
+++ tecton_client/utils/data_types.py
@@ -4,9 +4,7 @@ from typing import Self
 
 
 class DataType(abc.ABC):
-    """
-    Base DataType class
-    """
+    """Base DataType class. All data types inherit from this class."""
 
     def __str__(self: Self) -> str:
         # Require __str__ implementation. Used in error messages.
@@ -14,28 +12,36 @@ class DataType(abc.ABC):
 
 
 class IntType(DataType):
+    """Class to represent data of type Integer."""
+
     def __str__(self: Self) -> str:
         return "Int"
 
 
 class FloatType(DataType):
+    """Class to represent data of type Float."""
+
     def __str__(self: Self) -> str:
         return "Float"
 
 
 class StringType(DataType):
+    """Class to represent data of type String."""
 
     def __str__(self: Self) -> str:
         return "String"
 
 
 class BoolType(DataType):
+    """Class to represent data of type Boolean."""
 
     def __str__(self: Self) -> str:
         return "Bool"
 
 
 class ArrayType(DataType):
+    """Class to represent data of type Array. Can contain elements of another DataType."""
+
     def __init__(self: Self, element_type: DataType) -> None:
         self._element_type = element_type
 
@@ -48,6 +54,13 @@ class ArrayType(DataType):
 
 
 class StructField:
+    """Class to represent a field in a StructType.
+
+    Attributes:
+        name: Name of the field
+        data_type: DataType of the field
+    """
+
     def __init__(self: Self, name: str, data_type: DataType) -> None:
         self._name = name
         self._data_type = data_type
@@ -65,6 +78,12 @@ class StructField:
 
 
 class StructType(DataType):
+    """Class to represent data of type Struct.
+
+    Attributes:
+        fields: List of StructField objects, one for each field in the struct.
+    """
+
     def __init__(self: Self, fields: List[StructField]) -> None:
         self._fields = fields
 
