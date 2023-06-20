0a275a5e0e5c7a29ee5369ed1302573e06642943
Greeshma Karanth <greeshmakaranth.13@gmail.com>
2023-06-20 18:03:22 -0400

    Parametrizing get-features tests

--- tests/tecton_client_test.py
+++ tests/tecton_client_test.py
@@ -12,22 +12,29 @@ url = "https://thisisaurl.ai"
 
 
 class TestTectonClient:
-    @pytest.mark.asyncio
-    async def test_get_features(self: Self, httpx_mock: HTTPXMock) -> None:
-        httpx_mock.add_response(
-            json={
-                "result": {"features": ["1", 11292.571748310578]},
-                "metadata": {
-                    "features": [
-                        {
-                            "name": "transaction_amount_is_high.transaction_amount_is_high",
-                            "dataType": {"type": "int64"},
-                        },
-                        {"name": "transaction_distance_from_home.dist_km", "dataType": {"type": "float64"}},
-                    ]
+    mock_response1 = {
+        "result": {"features": ["1", 11292.571748310578]},
+        "metadata": {
+            "features": [
+                {
+                    "name": "transaction_amount_is_high.transaction_amount_is_high",
+                    "dataType": {"type": "int64"},
                 },
-            }
-        )
+                {"name": "transaction_distance_from_home.dist_km", "dataType": {"type": "float64"}},
+            ]
+        },
+    }
+    expected_response1 = {
+        "transaction_amount_is_high.transaction_amount_is_high": 1,
+        "transaction_distance_from_home.dist_km": 11292.571748310578,
+    }
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("mock_http_response, expected_response", [(mock_response1, expected_response1)])
+    async def test_get_features(
+        self: Self, httpx_mock: HTTPXMock, mock_http_response: dict, expected_response: dict
+    ) -> None:
+        httpx_mock.add_response(json=mock_http_response)
         tecton_client = TectonClient(url, api_key)
         join_key_map = {"test_join_key_1": "test_join_value_1", "test_join_key_2": "test_join_value_2"}
         request_context_map = {"test_request_context_1": 1234, "test_request_context_2": "test_string_value"}
@@ -39,9 +46,6 @@ class TestTectonClient:
             workspace_name="tecton-fundamentals-tutorial-live",
         )
         get_features_response = await tecton_client.get_features(get_features_request)
-        expected_response = {
-            "transaction_amount_is_high.transaction_amount_is_high": 1,
-            "transaction_distance_from_home.dist_km": 11292.571748310578,
-        }
+
         assert get_features_response.get_feature_values_dict() == expected_response
         await tecton_client.close()
