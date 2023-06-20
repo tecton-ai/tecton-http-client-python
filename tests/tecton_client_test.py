bbfe67da1b3688bad7487a456770b6356c004419
Greeshma Karanth <greeshmakaranth.13@gmail.com>
2023-06-20 18:04:43 -0400

    Grouping common code together

--- tests/tecton_client_test.py
+++ tests/tecton_client_test.py
@@ -29,23 +29,24 @@ class TestTectonClient:
         "transaction_distance_from_home.dist_km": 11292.571748310578,
     }
 
+    tecton_client = TectonClient(url, api_key)
+    join_key_map = {"test_join_key_1": "test_join_value_1", "test_join_key_2": "test_join_value_2"}
+    request_context_map = {"test_request_context_1": 1234, "test_request_context_2": "test_string_value"}
+
+    get_feature_request_data = GetFeatureRequestData(join_key_map, request_context_map)
+    get_features_request = GetFeaturesRequest(
+        feature_service_name="fraud_detection_feature_service",
+        request_data=get_feature_request_data,
+        workspace_name="tecton-fundamentals-tutorial-live",
+    )
+
     @pytest.mark.asyncio
     @pytest.mark.parametrize("mock_http_response, expected_response", [(mock_response1, expected_response1)])
     async def test_get_features(
         self: Self, httpx_mock: HTTPXMock, mock_http_response: dict, expected_response: dict
     ) -> None:
         httpx_mock.add_response(json=mock_http_response)
-        tecton_client = TectonClient(url, api_key)
-        join_key_map = {"test_join_key_1": "test_join_value_1", "test_join_key_2": "test_join_value_2"}
-        request_context_map = {"test_request_context_1": 1234, "test_request_context_2": "test_string_value"}
-
-        get_feature_request_data = GetFeatureRequestData(join_key_map, request_context_map)
-        get_features_request = GetFeaturesRequest(
-            feature_service_name="fraud_detection_feature_service",
-            request_data=get_feature_request_data,
-            workspace_name="tecton-fundamentals-tutorial-live",
-        )
-        get_features_response = await tecton_client.get_features(get_features_request)
+        get_features_response = await self.tecton_client.get_features(self.get_features_request)
 
         assert get_features_response.get_feature_values_dict() == expected_response
-        await tecton_client.close()
+        await self.tecton_client.close()
