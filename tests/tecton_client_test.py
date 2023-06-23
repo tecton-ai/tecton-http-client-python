5bb5c099d5a0264df6c8f5fccf251faede63345e
Greeshma Karanth <greeshmakaranth.13@gmail.com>
2023-06-23 11:38:12 -0400

    Adding more extensive tests

--- tests/tecton_client_test.py
+++ tests/tecton_client_test.py
@@ -21,8 +21,18 @@ class TestTectonClient:
         "test.output_int1": 24,
         "test.output_int2": 691,
     }
+    expected_response2 = {
+        "transaction_amount.is_present": True,
+        "merchant_fraud_rate.is_fraud_mean_90d_1d": None,
+        "user_transaction_counts.transaction_id_last_3_1d_1d": 669,
+        "user_transaction_amount_metrics.amt_mean_1d_10m": 842.8599999999999,
+        "user_transaction_counts.test": [
+            "892054b9598370dce846bb6e4b5805a1",
+            "cc9f13814a736160984bc9896222e4d9",
+            "43a9799c961de6ebb22c122d8c7eb340",
+        ],
+    }
 
-    tecton_client = TectonClient(url, api_key)
     join_key_map = {"test_join_key_1": "test_join_value_1", "test_join_key_2": "test_join_value_2"}
     request_context_map = {"test_request_context_1": 1234, "test_request_context_2": "test_string_value"}
 
@@ -34,13 +44,17 @@ class TestTectonClient:
     )
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize("file_name, expected_response", [("sample_response_mixed.json", expected_response1)])
+    @pytest.mark.parametrize(
+        "file_name, expected_response",
+        [("sample_response_mixed.json", expected_response1), ("sample_response_metadata.json", expected_response2)],
+    )
     async def test_get_features(self: Self, httpx_mock: HTTPXMock, file_name: str, expected_response: dict) -> None:
+        tecton_client = TectonClient(url, api_key)
         file_name = f"tests/test_data/{file_name}"
         with open(file_name) as json_file:
             json_response = json.load(json_file)
             httpx_mock.add_response(json=json_response)
-            get_features_response = await self.tecton_client.get_features(self.get_features_request)
+            get_features_response = await tecton_client.get_features(self.get_features_request)
 
         assert {k: v.feature_value for k, v in get_features_response.feature_values.items()} == expected_response
-        await self.tecton_client.close()
+        await tecton_client.close()
