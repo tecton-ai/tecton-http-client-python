3f9b185092833ed9d5a1f42e1d23fa908c47b89b
Greeshma Karanth <greeshmakaranth.13@gmail.com>
2023-06-27 12:17:06 -0400

    Changing datetime tests

--- tests/tecton_client_test.py
+++ tests/tecton_client_test.py
@@ -61,11 +61,11 @@ class TestTectonClient:
     }
 
     expected_metadata = [
-        (BoolType, FeatureStatus.PRESENT, 0),
-        (FloatType, FeatureStatus.MISSING_DATA, "2023-05-03T00:00:00"),
-        (IntType, FeatureStatus.PRESENT, "2023-05-04T00:00:00"),
-        (FloatType, FeatureStatus.PRESENT, "2023-05-04T15:50:00"),
-        (ArrayType, FeatureStatus.PRESENT, "2023-05-03T00:00:00"),
+        (BoolType, FeatureStatus.PRESENT, "2023-06-22T08:30:00+00:00"),
+        (FloatType, FeatureStatus.MISSING_DATA, "2023-05-03T00:00:00+00:00"),
+        (IntType, FeatureStatus.PRESENT, "2023-06-22T08:30:45+00:00"),
+        (FloatType, FeatureStatus.PRESENT, "2023-06-22T08:30:45+00:00"),
+        (ArrayType, FeatureStatus.PRESENT, "2023-06-22T08:30:45+00:00"),
     ]
     expected_slo_info = {
         "dynamodb_response_size_bytes": None,
