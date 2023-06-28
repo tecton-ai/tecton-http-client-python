7b043f6dd8027086a0271f83abfb65eb769c8585
Greeshma Karanth <greeshmakaranth.13@gmail.com>
2023-06-28 14:36:53 -0400

    Adding links :

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,5 +10,6 @@ def dict_equals(dict1: dict, dict2: dict) -> bool:
 
     """
     #     From Python version 3.6+, dictionary comparison happens agnostic of the order of the keys.
-    #     See [official documentation](https://docs.python.org/2/reference/expressions.html#not-in) for more details.
+    #     See [official documentation](https://docs.python.org/2/reference/expressions.html#value-comparisons) for
+    #     more information on value comparisons.
     return dict1 == dict2
