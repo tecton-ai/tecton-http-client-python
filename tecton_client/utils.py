2dd32687cc765d401519d00d970555df4d3e269a
Greeshma Karanth <greeshmakaranth.13@gmail.com>
2023-06-29 11:33:40 -0400

    Making return optional for parse-time

--- tecton_client/utils.py
+++ tecton_client/utils.py
@@ -1,19 +1,20 @@
 from datetime import datetime
 from typing import Final
 from typing import List
+from typing import Optional
 
 
 DATETIME_FORMATS: Final[List[str]] = ["%Y-%m-%dT%H:%M:%SZ", "%Y-%m-%dT%H:%M:%S.%fZ"]
 
 
-def parse_string_to_isotime(datetime_string: str) -> datetime:
+def parse_string_to_isotime(datetime_string: str) -> Optional[datetime]:
     """Parse a string into a datetime object.
 
     Args:
         datetime_string (str): The string to parse to datetime.
 
     Returns:
-        datetime: The parsed datetime object.
+        Optional[datetime]: The parsed datetime object if the string is parsed successfully, else None.
 
     """
     for format in DATETIME_FORMATS:
