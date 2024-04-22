2f0a5f570c048190bf02630ec006a2c0eb7eec4b
Mary Solbrig <mary.solbrig@tecton.ai>
2024-04-22 10:04:32 -0700

    Fix async "'AsyncClient.post' was never awaited" error in get AsyncTectonClient.get_feature_service_metadata (#61)
    Co-authored-by: Mary Solbrig <msolbrig@apple.com>

--- tests/test_async_client.py
+++ tests/test_async_client.py
@@ -1,16 +1,15 @@
 import json
-from unittest import TestCase
-from unittest.mock import MagicMock, patch
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import AsyncMock, MagicMock, patch
 
 import httpx
 from httpx import Headers
-from pytest import mark
 
 from tecton_client import AsyncTectonClient, MetadataOptions, RequestOptions
 from tecton_client.exceptions import NotFoundError
 
 
-class TestTectonClient(TestCase):
+class TestTectonClient(IsolatedAsyncioTestCase):
     def mockPatch(self, *args, **kwargs):
         patcher = patch(*args, **kwargs)
         self.addCleanup(patcher.stop)
@@ -45,12 +44,11 @@ class TestTectonClient(TestCase):
             url="https://fake.tecton.ai", api_key="fake-api-key", default_workspace_name="workspace", client=mock_client
         )
         mock_httpx_constructor.assert_not_called()
-        self.assertEquals(
+        self.assertEqual(
             mock_client.headers,
             {"authorization": "Tecton-key fake-api-key", "user-agent": "tecton-http-python-client 0.1.0test"},
         )
 
-    @mark.asyncio
     async def test_default_workspace_null(self):
         client = AsyncTectonClient(url="https://fake.tecton.ai", api_key="fake-api-key", client=self.mock_client)
 
@@ -63,9 +61,8 @@ class TestTectonClient(TestCase):
             workspace_name="override-workspace",
         )
         workspace = self.mock_client._request_log[0]["params"]["workspaceName"]
-        self.assertEquals(workspace, "override-workspace")
+        self.assertEqual(workspace, "override-workspace")
 
-    @mark.asyncio
     async def test_default_workspace(self):
         client = AsyncTectonClient(
             url="https://fake.tecton.ai",
@@ -76,9 +73,8 @@ class TestTectonClient(TestCase):
 
         await client.get_features(feature_service_name="fake-feature-service", join_key_map={"user_id": "id123"})
         workspace = self.mock_client._request_log[0]["params"]["workspaceName"]
-        self.assertEquals(workspace, "fake-workspace")
+        self.assertEqual(workspace, "fake-workspace")
 
-    @mark.asyncio
     async def test_override_workspace(self):
         client = AsyncTectonClient(
             url="https://fake.tecton.ai",
@@ -94,13 +90,14 @@ class TestTectonClient(TestCase):
         )
 
         workspace = self.mock_client._request_log[0]["params"]["workspaceName"]
-        self.assertEquals(workspace, "override-workspace")
+        self.assertEqual(workspace, "override-workspace")
 
-    @mark.asyncio
     async def test_get_features_encode(self):
         # using just magic_mock here in order to assert on client.post.assert_called_with
-        mock_http_client = MagicMock()
-        mock_http_client.post.return_value.json.return_value = {"result": {"features": []}}
+        mock_response = MagicMock()
+        mock_response.json.return_value = {"result": {"features": []}}
+        mock_http_client = AsyncMock()
+        mock_http_client.post.return_value = mock_response
         client = AsyncTectonClient(
             url="https://fake.tecton.ai",
             api_key="fake-api-key",
@@ -114,12 +111,11 @@ class TestTectonClient(TestCase):
             request_options=RequestOptions(read_from_cache=False),
         )
         mock_http_client.post.assert_called_with(
-            "https://fake.tecton.ai/api/v1/feature-service/get-features",
+            url="https://fake.tecton.ai/api/v1/feature-service/get-features",
             json={
                 "params": {
                     "workspaceName": "workspace",
                     "featureServiceName": "fake-feature-service",
-                    "featureServiceId": None,
                     "joinKeyMap": {"user_id": "id123"},
                     "requestContextMap": {},
                     "allowPartialResults": False,
@@ -135,15 +131,16 @@ class TestTectonClient(TestCase):
             },
         )
 
-    @mark.asycnio
     async def test_get_feature_service_metadata_encode(self):
         # using just magic_mock here in order to assert on client.post.assert_called_with
-        mock_http_client = MagicMock()
-        mock_http_client.post.return_value.json.return_value = {
+        mock_response = MagicMock()
+        mock_response.json.return_value = {
             "inputJoinKeys": [],
             "inputRequestContextKeys": [],
             "featureValues": [],
         }
+        mock_http_client = AsyncMock()
+        mock_http_client.post.return_value = mock_response
         client = AsyncTectonClient(
             url="https://fake.tecton.ai",
             api_key="fake-api-key",
@@ -163,20 +160,28 @@ class TestTectonClient(TestCase):
             },
         )
 
-    @mark.asyncio
     async def test_get_features_decode(self):
-        test_client = httpx.Client(
+        test_client = httpx.AsyncClient(
             transport=httpx.MockTransport(lambda request: httpx.Response(200, json={"result": {"features": []}}))
         )
         client = AsyncTectonClient(
             url="https://fake.tecton.ai", api_key="fake-api-key", default_workspace_name="workspace", client=test_client
         )
         resp = await client.get_features(feature_service_name="fake-feature-service", join_key_map={"user_id": "id123"})
-        self.assertEquals(resp.result.features, [])
+        self.assertEqual(resp.result.features, [])
 
-    @mark.asyncio
+    async def test_get_feature_service_metadata_decode(self):
+        test_client = httpx.AsyncClient(
+            transport=httpx.MockTransport(lambda request: httpx.Response(200, json={"featureValues": ["this"]}))
+        )
+        client = AsyncTectonClient(
+            url="https://fake.tecton.ai", api_key="fake-api-key", default_workspace_name="workspace", client=test_client
+        )
+        resp = await client.get_feature_service_metadata(feature_service_name="fake-feature-service")
+        self.assertEqual(resp.feature_values, ["this"])
+
     async def test_raise_error(self):
-        test_client = httpx.Client(
+        test_client = httpx.AsyncClient(
             transport=httpx.MockTransport(lambda request: httpx.Response(404, json={"result": {"features": []}}))
         )
         client = AsyncTectonClient(
