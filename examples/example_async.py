e80aaf84f734bef87bbe95e452a5ffe64ca8fd8f
Mary Solbrig <msolbrig@apple.com>
2024-04-22 08:47:22 -0700

    fix async get metadata

--- examples/example_async.py
+++ examples/example_async.py
@@ -1,12 +1,10 @@
 import asyncio
-import os
 
 from tecton_client import AsyncTectonClient, MetadataOptions
 
 my_url = "https://explore.tecton.ai/"
 workspace = "prod"
-my_api_key = os.environ.get("TECTON_API_KEY")
-
+my_api_key = "ada955cefef2e6e003c9a7477d514d4d"
 
 async_client = AsyncTectonClient(url=my_url, api_key=my_api_key, default_workspace_name=workspace)
 
@@ -24,6 +22,17 @@ async def call_api_ten_times():
     responses = await asyncio.gather(*requests)
     for resp in responses:
         print(resp.result.features)
+
+    # also call get_feature_service_metadata
+    requests = [
+        async_client.get_feature_service_metadata(
+            feature_service_name="fraud_detection_feature_service:v2",
+        )
+        for i in range(10)
+    ]
+    responses = await asyncio.gather(*requests)
+    for resp in responses:
+        print(resp)
 
 
 asyncio.run(call_api_ten_times())
